83f546952afe979de967ca0d1584ad3e465d524f
diegoale714 <diegodepas300@gmail.com>
2022-12-05 09:56:34 -0300

    ez

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://back-vercel-v3.onrender.com',
+  baseURL: 'https://vercel-back3.onrender.com',
 })
 
 export default instance
